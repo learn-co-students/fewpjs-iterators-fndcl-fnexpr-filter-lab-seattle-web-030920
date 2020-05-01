0678234800d5a7b714335306b4022940aaf33652
ShunshunL <shunshunlyu@protonmail.com>
2020-05-01 01:15:47 -0700

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
 // Code your solution here
+function findMatching(source, sought) {
+  return source.filter( possibleMatch =>
+    possibleMatch.toLowerCase() === sought.toLowerCase()
+  )
+}
+
+function fuzzyMatch(source, testString) {
+  return source.filter( possibleMatch =>
+    possibleMatch.toLowerCase().indexOf(testString.toLowerCase()) === 0
+  )
+}
+
+function matchName(a, b){
+  return a.filter( record => record.name === b)
+}
