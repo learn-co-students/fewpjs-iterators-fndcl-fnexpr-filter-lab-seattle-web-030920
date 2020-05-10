f71859ec749100e2bd57b9079a62d775a835a10b
Theouty <aman.david.michael@gmail.com>
2020-05-10 01:11:45 -0700

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
-// Code your solution here
+function findMatching(array, string){
+  return  array.filter(driver => driver.toUpperCase() === string.toUpperCase())
+}
+
+function fuzzyMatch(array, string){
+    return array.filter(driver => {
+        let length = string.length
+        return driver.slice(0, length).toUpperCase() === string.toUpperCase()
+    })
+}
+
+function matchName(array, string){
+    return array.filter(element => {
+        return element.name.toUpperCase() === string.toUpperCase()
+    })
+}
